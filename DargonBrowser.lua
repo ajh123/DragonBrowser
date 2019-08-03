2db133ecc7d99e27c5ea348275e1127d26f21e28
ajh123 <41990982+ajh123@users.noreply.github.com>
2019-08-03 16:54:21 +0100

    Update DargonBrowser.lua

--- DargonBrowser.lua
+++ DargonBrowser.lua
@@ -5,7 +5,27 @@ code = ""
 page = {}
 count = 1
 centered = false
-hFile = fs.open("httptest.html", "r")
+
+web = io.read()
+
+local function get(repoFile,saveTo)
+local download = http.get("https://raw.github.com/ajh123/DragonBrowser/web/repoFile") --This will make 'download' hold the contents of the file.
+if download then --checks if download returned true or false
+   local handle = download.readAll() --Reads everything in download
+   download.close() --remember to close the download!
+   local file = fs.open(saveTo,"w") --opens the file defined in 'saveTo' with the permissions to write.
+   file.write(handle) --writes all the stuff in handle to the file defined in 'saveTo'
+   file.close() --remember to close the file!
+  else --if returned false
+   print("Unable to download the file "..repoFile)
+   print("Make sure you have the HTTP API enabled or")
+   print("an internet connection!")
+  end --end the if
+end --close the function
+
+get(web, "web.html")
+
+hFile = fs.open(web, "r")
 -- Read all file
 while true do
         h = hFile.readLine()
